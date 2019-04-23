c5ab3bfccbb78a376702606a49ac10aaa4e017d5
Posture <Posture_Cc@163.com>
2019-04-23 19:28:32 +0800

    sub

--- eureka-client-port2/src/main/java/com/chan/EurekaClientPort2Application.java
+++ eureka-client-port2/src/main/java/com/chan/EurekaClientPort2Application.java
@@ -2,12 +2,14 @@ package com.chan;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients//调用者启动时，打开Feign开关
+@ServletComponentScan//拦截器需要此注解才能生效
 public class EurekaClientPort2Application {
 
     public static void main(String[] args) {
